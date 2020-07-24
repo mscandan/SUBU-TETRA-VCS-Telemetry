e1b4ada98ad65000bfcef607c120ce497ebc24ab
Mehmet Selçuk Candan <mehmetselcukcandan@icloud.com>
2020-07-24 23:09:22 +0300

    initial setup

--- SubuTetraTelemetri/Form1.Designer.cs
+++ SubuTetraTelemetri/Form1.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SUBU - TETRA TELEMETRI 2020";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
